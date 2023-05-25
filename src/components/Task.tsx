975d39b987d77c690379bb52246c0e6b6639756b
Suzan Rana <dev.suzanrana@gmail.com>
2023-05-25 10:25:02 +0545

    Feat: Playing with defaults

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -34,28 +34,39 @@ const Task = ({ task, index, columnId }: Props) => {
   return (
     <>
       <Draggable draggableId={task.id.toString()} index={index}>
-        {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
-          <div
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-            className={`px-4 py-3 my-4 relative flex items-center justify-between  rounded-md transition-colors duration-500 ${
-              snapshot.isDragging ? "bg-green-600 text-black" : "bg-zinc-900"
-            } `}
-          >
-            {task.content}
-            <BsThreeDotsVertical
-              onClick={(e: React.MouseEvent<SVGElement, MouseEvent>) => {
-                handleMenuClick(e, task.id);
-              }}
-            />
-            {openSideMenu && (
-              <div ref={sideMenuRef}>
-                <SideMenu taskContent={task.content} setOpenSideMenu={setOpenSideMenu} columnId={columnId} taskId={task.id} />
-              </div>
-            )}
-          </div>
-        )}
+        {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => {
+          const transformedStyles = {
+            ...provided.draggableProps.style,
+          };
+          return (
+            <div
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              className={`px-4 relative py-3 my-4 flex items-center justify-between  rounded-md transition-colors duration-500 ${
+                snapshot.isDragging ? "bg-green-600 text-black" : "bg-zinc-900"
+              } `}
+              style={transformedStyles}
+            >
+              {task.content}
+              <BsThreeDotsVertical
+                onClick={(e: React.MouseEvent<SVGElement, MouseEvent>) => {
+                  handleMenuClick(e, task.id);
+                }}
+              />
+              {openSideMenu && (
+                <div ref={sideMenuRef}>
+                  <SideMenu
+                    taskContent={task.content}
+                    setOpenSideMenu={setOpenSideMenu}
+                    columnId={columnId}
+                    taskId={task.id}
+                  />
+                </div>
+              )}
+            </div>
+          );
+        }}
       </Draggable>
     </>
   );
--- src/components/TaskColumn.tsx
+++ src/components/TaskColumn.tsx
@@ -32,32 +32,38 @@ const TaskColumn = ({
   return (
     <>
       <Droppable droppableId={columnId}>
-        {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) => (
-          <article
-            className={`flex flex-col min-w-[300px] overflow-y-auto grow w-[32%] border-[2px]  transition-colors  rounded-lg   py-6 px-6 pb-16 ${
-              snapshot.isDraggingOver
-                ? "border-green-500 bg-zinc-900"
-                : "bg-slate-800"
-            }`}
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            <h3 className="text-center font-bold mb-4 mt-2 text-xl capitalize">
-              {column.title}
-            </h3>
-            {column.title === "To do" && <Input placeholder="Create new todo" cb={handleAddNewTodo} />}
-
-            {taskLists.map((task, index) => (
-              <Task
-                columnId={columnId}
-                key={task.id}
-                task={task}
-                index={index}
-              />
-            ))}
-            {provided.placeholder}
-          </article>
-        )}
+        {(provided: DroppableProvided, snapshot: DroppableStateSnapshot) =>
+        {
+          return  (
+            <article
+              className={`flex flex-col relative min-w-[300px] overflow-y-auto grow w-[32%] border-[2px]  transition-colors  rounded-lg   py-6 px-6 pb-16 ${
+                snapshot.isDraggingOver
+                  ? "border-green-500 bg-zinc-900"
+                  : "bg-slate-800"
+              }`}
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              <h3 className="text-center font-bold mb-4 mt-2 text-xl capitalize">
+                {column.title}
+              </h3>
+              {column.title === "To do" && (
+                <Input placeholder="Create new todo" cb={handleAddNewTodo} />
+              )}
+  
+              {taskLists.map((task, index) => (
+                <Task
+                  columnId={columnId}
+                  key={task.id}
+                  task={task}
+                  index={index}
+                />
+              ))}
+              {provided.placeholder}
+            </article>
+          )
+        }
+       }
       </Droppable>
     </>
   );
